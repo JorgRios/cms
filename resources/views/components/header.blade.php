8e37f117059a295a0067ce0b1e14368c61aeb3f0
Elverbecerra <elivertserpientes@gmail.com>
2022-03-28 18:50:10 -0400

    Se implemento un nuevo diseño del frontend

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,11 +3,10 @@
         <div class="row gx-5 align-items-center justify-content-center">
             <div class="col-lg-8 col-xl-7 col-xxl-6">
                 <div class="my-5 text-center text-xl-start">
-                    <h1 class="display-5 fw-bolder text-white mb-2">A Bootstrap 5 template for modern businesses</h1>
-                    <p class="lead fw-normal text-white-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                    <h1 class="display-5 fw-bolder text-white mb-2">Nuestra tienda G&D ofrece productos de telefonia celular de toda gama</h1>
+                    <p class="lead fw-normal text-white-50 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae hic soluta iure excepturi, accusantium quasi aspernatur quae consequuntur cum rerum autem quod. Vero porro, asperiores eum veniam at reprehenderit repudiandae.</p>
                     <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                        <a class="btn btn-dark btn-lg px-4 me-sm-3" href="#features">Get Started</a>
-                        <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
+                        <a class="btn btn-dark btn-lg px-4 me-sm-3" href="#features">Ver mas </a>
                     </div>
                 </div>
             </div>
